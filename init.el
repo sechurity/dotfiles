7283426544adb9f34210cad85b59bdf51e449e1d
William <william@fedora-laptop.lan>
2021-01-22 20:07:19 +0800

    testing

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; after git tutorial
 ;; binding x to be easier to hit
 (define-key key-translation-map [?\C-x] [?\C-t])
 (define-key key-translation-map [?\C-t] [?\C-x])
@@ -61,7 +62,10 @@
  '(ido-mode 'both nil (ido))
  '(line-move-visual t)
  '(line-number-mode t)
- '(package-selected-packages '(vlf)))
+ '(package-selected-packages '(vlf))
+ '(tool-bar-max-label-size 304)
+ '(tool-bar-position 'top)
+ '(tool-bar-style 'image))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
